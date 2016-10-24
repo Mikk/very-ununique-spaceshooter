7afcc69f14648136c9124ebc7959e4ffaf7c4934
mattiaslass <mattias.lass@gmail.com>
2016-10-24 10:10:42 +0300

    boss

---
+++ Assets/Scripts/BossMovement.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossMovement : MonoBehaviour {
+
+
+	public float xSpeed = 0;
+	public float ySpeed = -0.5f;
+	private float timeInPlay = 0;
+	private bool switched = false;
+
+	public bool bodyDestroyed = false;
+	public bool gunDestroyed = false;
+	public bool gun1Destroyed = false;
+	public bool gun2Destroyed = false;
+
+
+
+	void Start () {
+		transform.position = new Vector3(0, 7, 0);
+	}
+
+
+	void Update () {
+
+		if (bodyDestroyed || gunDestroyed && gun1Destroyed && gun2Destroyed)
+		{
+			Debug.Log("GAMEOVER");
+			Destroy(gameObject);
+		}
+
+		float time = Time.deltaTime;
+		float screenHeight = Camera.main.orthographicSize;
+		float screenWidth = screenHeight * Screen.width / Screen.height;
+
+		Vector3 pos = transform.position;
+
+		pos.y += time * ySpeed;
+		pos.x += time * xSpeed;
+		
+		if (pos.x >= screenWidth - 1)
+		{
+			xSpeed = -xSpeed;
+			pos.x = 2 * (screenWidth - 1) - pos.x;
+		}
+		else if (pos.x <= -screenWidth + 1)
+		{
+			pos.x = 2 * (-screenWidth + 1) - pos.x;
+			xSpeed = -xSpeed;
+		}
+
+		transform.position = pos;
+
+		timeInPlay += time;
+		if (!switched && timeInPlay > 4)
+		{
+			xSpeed = 1;
+			ySpeed = 0;
+			switched = true;
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Health : MonoBehaviour {
+
+	public int health;
+	public int type;
+	void ApplyDamage(int damage)
+	{
+		health -= damage;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (health <= 0)
+		{	
+			if (type == 0)
+			{
+				GetComponentInParent<BossMovement>().bodyDestroyed = true;
+			}
+			if (type == 1)
+			{
+				GetComponentInParent<BossMovement>().gun1Destroyed = true;
+			}
+			if (type == 2)
+			{
+				GetComponentInParent<BossMovement>().gun2Destroyed = true;
+			}
+			if (type == 3)
+			{
+				GetComponentInParent<BossMovement>().gunDestroyed = true;
+			}
+			Destroy(gameObject);
+		}
+	}
+}
